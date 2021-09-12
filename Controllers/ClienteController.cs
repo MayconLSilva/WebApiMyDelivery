0a406bada2b321dbaee13174955dd8fc6c9f64ef
Maycon Silva <mayconsilva_cp@icloud.com>
2021-09-11 22:48:28 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using Microsoft.Data.SqlClient;
+//using System.Web.Http;
+
+namespace WebAPIMyDelivery
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClienteController : Controller
+    {
+
+        private IConfiguration _config;
+        public ClienteController(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        [HttpGet("todos")]
+        public IEnumerable<ModelCliente> GetEstados()
+        {
+            using (SqlConnection conexao = new SqlConnection(
+            _config.GetConnectionString("ExemplosDapper")))
+            {
+                return conexao.Query<ModelCliente>(
+                "SELECT E.SiglaEstado, E.NomeEstado, E.NomeCapital, " +
+                "R.NomeRegiao " +
+                "FROM dbo.Estados E " +
+                "INNER JOIN dbo.Regioes R ON R.IdRegiao = E.IdRegiao " +
+                "ORDER BY E.NomeEstado");
+            }
+        }
+
+        [HttpGet("dd")]
+        public System.Web.Http.IHttpActionResult gettets()
+        {
+            using (SqlConnection conexao = new SqlConnection(
+            _config.GetConnectionString("ExemplosDapper")))
+            {
+                var ersultado = conexao.Query<ModelCliente>(
+                "SELECT E.SiglaEstado, E.NomeEstado, E.NomeCapital, " +
+                "R.NomeRegiao " +
+                "FROM dbo.Estados E " +
+                "INNER JOIN dbo.Regioes R ON R.IdRegiao = E.IdRegiao " +
+                "ORDER BY E.NomeEstado");
+                
+                return (System.Web.Http.IHttpActionResult)ersultado;
+            }
+
+        }
+
+    }
+}
---
+++ Model/ModelCliente.cs
@@ -0,0 +1,26 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace WebAPIMyDelivery
+{
+    [Table("Cliente")]
+    public class ModelCliente
+    {
+        [ExplicitKey]
+        public int id { get; set; }
+        public string nomeFantasia { get; set; }
+        public string apelidoRazao { get; set; }
+        public string cpfCnpj { get; set; }
+        public string rgIE { get; set; }
+        public string telefone { get; set; }
+        public string celular { get; set; }
+        public string email { get; set; }
+
+    }
+}
+
+
